247c940d4d5fc034bf44bd5437977721cef097b4
naim <mdnaimhasan.official@gmail.com>
2026-07-30 00:46:11 +0600

    Second Problem Solve

--- assignment.js
+++ assignment.js
@@ -18,7 +18,34 @@ let input2 = matchWinner(1,3);
 let input3 = matchWinner(2,2);
 let input4 = matchWinner("3",2);
 
-console.log(input1);
-console.log(input2);
-console.log(input3);
-console.log(input4);
+// console.log(input1);
+// console.log(input2);
+// console.log(input3);
+// console.log(input4);
+
+
+// Second Problem
+
+function isElevatorSafe(weights) {
+    // Write your code here
+    let totalWeight = 0;
+    for(let weight of weights){
+        // console.log(weight);
+        totalWeight += weight;
+        // console.log(totalWeight);
+    }
+    // return totalWeight;
+    if(!Array.isArray(weights)){
+            return "Invalid"
+        }else if(totalWeight <= 400){
+            return true;
+        }else{
+            return false
+        }
+    
+
+}
+console.log(isElevatorSafe([60, 75, 50]));
+console.log(isElevatorSafe([90, 100, 95, 120]));
+console.log(isElevatorSafe([400]));
+console.log(isElevatorSafe("60,75,50"));
